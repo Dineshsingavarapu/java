963c53309c46ce8ee235ce4bdd165790d7ce252e
dineshsingavarapu <151906343+Dineshsingavarapu@users.noreply.github.com>
2024-11-05 12:34:02 +0530

    Create String is immutable.java

---
+++ String is immutable.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Main {
+    public static void main(String args[]) {
+        String s1 = "Dinesh";
+        s1 = s1.concat("Singavarapu");
+        System.out.println("String s1 is:"+s1);
+        String s2 = "Dinesh";
+        String s3 = s2.concat("Singavarapu");
+        System.out.println("String s3 is:"+s3);
+    }
+}
