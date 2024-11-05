3ac9ac670959d766b4bb4c90d8f0d44ff93e781e
dineshsingavarapu <151906343+Dineshsingavarapu@users.noreply.github.com>
2024-11-05 11:34:58 +0530

    Create to find area of circle.java

---
+++ to find area of circle.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    double r;
+    double area;
+    {
+        return(22/7.0)*r*r;
+    }
+    void setCircle(double c){
+        r=c;
+    }
+}
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter radius of the circle");
+        double radius=sc.nextdouble
+    }
+}
